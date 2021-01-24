279d763b48cd9f9fc20fe394f6a0a364319027cc
louis <louisduong05@gmail.com>
2021-01-24 19:03:16 +0700

    git init 7

--- app/services/export_filter.rb
+++ app/services/export_filter.rb
@@ -10,10 +10,10 @@ class ExportFilter
   def result
     @exports = @exports.where(user_id: user_id) if user_id.present?
     @exports = @exports.where(pays: { status: status }) if status.present?
-    @imports = @exports.where(
+    @exports = @exports.where(
       ["created_at >= ?", from.to_date.beginning_of_day]
     ) if from.present?
-    @imports = @exports.where(
+    @exports = @exports.where(
       ["created_at <= ?", to.to_date.end_of_day]
     ) if to.present?
     @exports
